a0baca4d792fad13351993901d1991b726b284f2
Andrey Sergeev <andreyinoks93@yandex.ru>
2019-06-21 01:07:38 +0700

    Сделал секцию с формой и футер

--- src/js/navbar-menu.js
+++ src/js/navbar-menu.js
@@ -1,5 +1,7 @@
+$(document).ready(function() {
 $('.burger__button').on('click', function(e){
   e.preventDefault;
   $(this).toggleClass('burger__button_active');
   $('.burger__item').toggleClass('burger__item_active');
+});
 });
