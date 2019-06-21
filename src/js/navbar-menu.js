db7303617f516d5a33540d5c8156559b9b83f08b
Andrey Sergeev <andreyinoks93@yandex.ru>
2019-06-21 22:56:50 +0700

    Сделал якорные ссылки на меню с плавной прокруткой

--- src/js/navbar-menu.js
+++ src/js/navbar-menu.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
+// Меню для мобильных телефонов
 $('.burger__button').on('click', function(e){
   e.preventDefault;
   $(this).toggleClass('burger__button_active');
   $('.burger__item').toggleClass('burger__item_active');
 });
+// Якорные ссылки
+$(".navbar-list__text").click(function () {
+  var elementClick = $(this).attr("href")
+  var destination = $(elementClick).offset().top;
+  jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
+  return false;
+  });
 });
