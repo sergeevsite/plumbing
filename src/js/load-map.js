d81c1941ae76096e002467c8810d490711b56cf5
Andrey Sergeev <andreyinoks93@yandex.ru>
2019-06-22 23:22:38 +0700

    Добавил 2 модальных окна, обратный звонок и подать заявку

--- src/js/load-map.js
+++ src/js/load-map.js
@@ -3,7 +3,7 @@ var newsTop = news.offset().top;
 $(window).bind('scroll', function(){
   var windowTop = $(this).scrollTop();
   if (windowTop > newsTop) {
-    $('#map').html('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A9be676bab37bd38be921d1531fa97f706de84b746655eb47f497f821804e220a&amp;width=100%25&amp;height=666&amp;lang=ru_RU&amp;scroll=false&amp;apikey=c2c92151-9461-4524-b99a-8c545a54b5ca""></script>')
+    $('#map').html('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A9be676bab37bd38be921d1531fa97f706de84b746655eb47f497f821804e220a&amp;width=100%25&amp;height=666&amp;lang=ru_RU&amp;scroll=false&amp;apikey=c2c92151-9461-4524-b99a-8c545a54b5ca"></script>')
     $(window).unbind('scroll');
   }
   
--- src/js/navbar-menu.js
+++ src/js/navbar-menu.js
@@ -12,4 +12,44 @@ $(".navbar-list__text").click(function () {
   jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
   return false;
   });
+$(".burger__item").click(function () {
+  var elementClick = $(this).attr("href")
+  var destination = $(elementClick).offset().top;
+  jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
+  return false;
+  });
+  
+// Модальное окно
+    var button = $('#button');
+    var close = $('#close');
+    var modal = $('#modal');
+  
+    button.on('click', function() {
+      modal.addClass('modal_active');
+    });
+    close.on('click', function() {
+      modal.removeClass('modal_active');
+    });
+
+    var closeScs = $('#close-scs');
+    var success= $('#success');
+
+    $('.modal-dialog__button').on('click', function(e) {
+      e.preventDefault();
+      success.addClass('success_active');
+      modal.removeClass('modal_active');
+    });
+    $('.form__button').on('click', function(e) {
+      e.preventDefault();
+      success.addClass('success_active');
+      modal.removeClass('modal_active');
+    });
+    closeScs.on('click', function(e) {
+      e.preventDefault();
+      success.removeClass('success_active');
+
+    });
+    setInterval(function() {
+      success.removeClass('success_active');
+    }, 3000 );
 });
